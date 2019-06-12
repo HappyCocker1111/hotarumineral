f879bb8fbb77ba54b0b358b578bda48bb5a0444b
弥和 <a.cokegasuki@gmail.com>
2019-06-12 16:59:57 +0900

    SNSボタン　埋め込み

--- wp-content/themes/griddist-child/functions.php
+++ wp-content/themes/griddist-child/functions.php
@@ -1,10 +1,12 @@
 <?php
+//子テーマとしての利用宣言
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );//親テーマのCSS
     wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/custom-style.css', array('parent-style')); // 子テーマのcss
 }
 
+//カスタム投稿の表示件数設定
 function change_posts_per_page($query) {
     if ( is_admin() || ! $query->is_main_query() ) {
       return;
@@ -16,3 +18,6 @@ function change_posts_per_page($query) {
     }
   }
   add_action( 'pre_get_posts', 'change_posts_per_page' );
+
+//ウィジェットでショートコードを使う
+add_filter('widget_text','do_shortcode');
